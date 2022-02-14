40c07b04a48355de2fe710f389b969d95bae0df5
Marco Dall'Omo <marcodallomo@marcos-mbp.home>
2022-02-14 18:31:43 +0000

    first commit

---
+++ src/components/Start.jsx
@@ -0,0 +1,21 @@
+import React, { useRef } from "react";
+
+const Start = ({ setUsername }) => {
+  const inputRef = useRef();
+
+  const handleClick = () => {
+    inputRef.current.value && setUsername(inputRef.current.value);
+  };
+
+  return (
+    <div className="start">
+      <img src="images/title.jpg" alt="Milionaire" width="400" className="imageStart" />
+      <input placeholder=" Enter your name" className="startInput" ref={inputRef} />
+      <button className="startButton" onClick={handleClick}>
+        Start
+      </button>
+    </div>
+  );
+};
+
+export default Start;
